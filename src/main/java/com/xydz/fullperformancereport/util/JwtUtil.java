e739ab60cbc0ef2f8eb86d5252939b81449f0de8
xiachenchen <2539177130@qq.com>
2023-02-18 15:21:04 +0800

    jwt修复

--- src/main/java/com/xydz/fullperformancereport/util/JwtUtil.java
+++ src/main/java/com/xydz/fullperformancereport/util/JwtUtil.java
@@ -1,6 +1,5 @@
 package com.xydz.fullperformancereport.util;
 
-
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.jwt.JWT;
 import com.xydz.fullperformancereport.pojo.entity.User;
@@ -15,6 +14,11 @@ public class JwtUtil {
 
     public static final byte[] SECRET = "SECRET".getBytes();
 
+    /**
+     * 根据用户创建token
+     * @param user
+     * @return
+     */
     public static String createToken(User user){
         String token = JWT.create()
                 .setExpiresAt(DateUtil.offsetDay(DateUtil.date(),3))
@@ -24,10 +28,21 @@ public class JwtUtil {
         return token;
     }
 
+    /**
+     * 验证token
+     * @param token
+     * @return
+     */
     public static boolean verify(String token){
         return JWT.of(token).setKey(SECRET).validate(0);
     }
 
+    /**
+     * 获取token中的信息
+     * @param token
+     * @param dataName
+     * @return
+     */
     public static String getData(String token,String dataName){
         return (String)JWT.of(token).getPayload(dataName);
     }
