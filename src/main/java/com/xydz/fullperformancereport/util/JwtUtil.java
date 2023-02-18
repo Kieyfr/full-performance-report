4f1494d8bfa75db6fe7a9ea34dd46a4b05d72418
xiachenchen <2539177130@qq.com>
2023-02-18 14:05:34 +0800

    jwt修复

--- src/main/java/com/xydz/fullperformancereport/util/JwtUtil.java
+++ src/main/java/com/xydz/fullperformancereport/util/JwtUtil.java
@@ -1,50 +1,44 @@
 package com.xydz.fullperformancereport.util;
 
 
-import cn.hutool.crypto.SecureUtil;
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.jwt.JWT;
-import cn.hutool.jwt.JWTUtil;
+import cn.hutool.jwt.JWTValidator;
+import cn.hutool.jwt.signers.JWTSignerUtil;
 import com.xydz.fullperformancereport.pojo.entity.User;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @Author xiachenchen
  * @ClassName: JwtUtil
  * @Description: token解析工具类
  * @Date 2022/12/14
-*/
+ */
 public class JwtUtil {
 
     public static final byte[] SECRET = "SECRET".getBytes();
 
-    public static Map<String, Object> getMap(User user){
-        Map<String, Object> map = new HashMap<String, Object>() {
-            private static final long serialVersionUID = 1L;
-            {
-                put("id", user.getUserId());
-                if(user.getUserPassword().equals(SecureUtil.md5(user.getUserId()))){
-                    put("expire_time", System.currentTimeMillis() + 1000);
-                }else{
-                    put("expire_time", System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 15);
-                }
-
-            }
-        };
-        return map;
-    }
-
     public static String createToken(User user){
-        return JWTUtil.createToken(getMap(user), SECRET);
+        String token = JWT.create()
+                .setExpiresAt(DateUtil.offsetDay(DateUtil.date(),3))
+                .setPayload("id", user.getUserId())
+                .setKey(SECRET)
+                .sign();
+        return token;
     }
 
     public static boolean verify(String token){
-        return JWTUtil.verify(token,SECRET);
+        try {
+            JWTValidator.of(token)
+                    .validateDate()
+                    .validateAlgorithm(JWTSignerUtil.hs256(SECRET));
+        }catch (Exception e){
+            return false;
+        }
+        return true;
     }
 
     public static String getData(String token,String dataName){
-        JWT jwt = JWTUtil.parseToken(token);
+        JWT jwt = JWT.of(token);
         return (String)jwt.getPayload(dataName);
     }
 
